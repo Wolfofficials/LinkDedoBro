f603d63523a0eec1f7dbeb08e0a5da1940b6fd80
Wolfofficials <92360356+Wolfofficials@users.noreply.github.com>
2023-05-23 01:44:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def send_message(client, joined_groups):
 async def main():
     api_id = int(os.getenv('API_ID'))
     api_hash = os.getenv('API_HASH')
-    session_name = os.getenv('SESSION_NAME')
+
     session_string = os.getenv('SESSION_STRING')
 
     client = Client(session_name=session_name, api_id=api_id, api_hash=api_hash, session_string=session_string)
